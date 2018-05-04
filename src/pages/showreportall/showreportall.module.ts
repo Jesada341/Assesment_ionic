4be9fae8381705c305b6343edef852696c1ca549
prachaya ruangprach <58160124@go.buu.ac.th>
2018-05-04 19:26:17 +0700

    Edit by Prachaya 4/05/2561

---
+++ src/pages/showreportall/showreportall.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ShowreportallPage } from './showreportall';
+
+@NgModule({
+  declarations: [
+    ShowreportallPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ShowreportallPage),
+  ],
+})
+export class ShowreportallPageModule {}
