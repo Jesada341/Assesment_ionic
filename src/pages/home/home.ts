c101300a122b00ea39b49c64be141926de25fed8
Jesada <58160341@go.buu.ac.th>
2018-05-04 20:21:19 +0700

    Simple login

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -10,9 +10,27 @@ export class HomePage {
   username:string;
   password:string;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController) {
     this.username = '';
     this.password = '';
   }
-
+  
+  showAlert(user: string,pass: string){
+    //console.log('User: '+user+'PAss: '+pass);
+    if(user != '' && user == 'admin'){
+      let alert = this.alertCtrl.create({
+        title: 'สวัสดี',
+        subTitle: 'ยินดีต้อนรับ ['+user+'] เข้าสู่ระบบประเมิน, สามารถตรวจสอบรายการของท่านได้เลย!',
+        buttons: ['รับทราบ']
+      });
+      alert.present();
+    }else{
+      let alert = this.alertCtrl.create({
+        title: 'เข้าสู่ระบบไม่สำเร็จ',
+        subTitle: 'กรุณากรอกรหัสเข้าใช้งานให้ถูกต้อง',
+        buttons: ['OK']
+      });
+      alert.present();
+    }
+  }
 }
