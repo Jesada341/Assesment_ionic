ff463452e88e26cd3a5c0baf76347a1e77fa8301
kontee <58160647@go.buu.ac.th>
2018-05-18 21:26:32 +0700

    DoAssessment Shew_data & Select

--- src/pages/doassess/doassess.ts
+++ src/pages/doassess/doassess.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { LoginProvider } from './../../providers/login/login';
 
 /**
  * Generated class for the DoassessPage page.
@@ -14,12 +15,44 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'doassess.html',
 })
 export class DoassessPage {
+  acYear: number;
+  acTerm: number;
+  public result: Evu[];
+  constructor(public navCtrl: NavController, public navParams: NavParams, private score: LoginProvider) {
+    this.acYear = this.navParams.get('acYear');
+    this.acTerm = this.navParams.get('acTerm');
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    // this.score.CheckStatusAssest(this.acYear,this.acTerm).then((data: any) => {
+    //   console.log(data);
+    // });
+
+    this.score.get_score(this.acYear, this.acTerm).then((data: any) => {
+      console.log(data);
+      this.result = data;
+    });
+
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad DoassessPage');
+
+    console.log(this.acYear + " " + this.acTerm);
   }
 
+}
+
+interface Evu {
+  Evu_name: string;
+  Evu_year: string;
+  Evu_term: string;
+  Evu_data: Topic[];
+}
+
+interface Topic {
+  Topic_name: string;
+  Sub_data: Sub[];
+}
+
+interface Sub {
+  Sub_name: string;
+  Point: string;
 }
--- src/providers/login/login.ts
+++ src/providers/login/login.ts
@@ -32,4 +32,15 @@ export class LoginProvider {
     })
 
   }
+
+  get_score(acYear: any, acTerm: any) {
+    return new Promise((resolve, reject) => {
+      let url = "http://10.80.39.17/TSP58/nursing/application/controllers/amis/Mobile/Ionic/sqlreport.php?acYear=" + 2560 + "&acTerm=" + 1;
+      this.http.get(url)
+        .map(res => res.json()).subscribe(data => {
+          resolve(data);
+        })
+    });
+    // console.log(this.http.get(url));
+  }
 }
